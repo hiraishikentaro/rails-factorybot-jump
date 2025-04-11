5b76df729423b2403805a72d5f994045ec939823
hiraishikentaro <silva.ken.97@gmail.com>
2025-04-11 17:20:59 +0900

    refactor: move FactoryLinkProvider to providers directory

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,6 +1,6 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
-import { FactoryLinkProvider } from "../../extension";
+import { FactoryLinkProvider } from "../../providers/factoryLinkProvider";
 import * as path from "path";
 import * as os from "os";
 import * as fs from "fs";
